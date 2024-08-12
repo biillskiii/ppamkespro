bc46ae05c17b4037f9a32835472be8e443d6818d
veda <vedabezaleel@gmail.com>
2024-08-13 03:46:21 +0700

    feat: SideBar Componen

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,14 +1,20 @@
+'use client';
+
 import React, { useState } from "react";
 import {
   IoMdArrowRoundBack,
   IoIosArrowDown,
   IoIosArrowForward,
 } from "react-icons/io";
-import { FaCircleCheck } from "react-icons/fa";
+import { FaCircleCheck } from "react-icons/fa6";
 
 const Sidebar = () => {
   const [activeAccordion, setActiveAccordion] = useState(null);
   const [activeSection, setActiveSection] = useState(null);
+  const [sesiAktif, setSesiAktif] = useState({
+    Bagian: 0,
+    Assesmen: 0
+  });
   const toggleAccordion = (section) => {
     if (activeAccordion === section) {
       setActiveAccordion(null);
@@ -20,6 +26,150 @@ const Sidebar = () => {
   const handleSectionClick = (section) => {
     setActiveSection(section);
   };
+
+  const menus = [
+    {
+      title: "Bagian 0",
+      href: "/assesstment/bagian-0",
+    },
+    {
+      title: "Bagian I",
+      href: "/assesstment/bagian-1",
+      subMenu: [
+        {
+          title:
+            "Asesmen 1",
+          desc: "Pertanyaan 1–7",
+          href: "/assesstment/bagian-0",
+        },
+        {
+          title:
+            "Asesmen 2",
+          desc: "Pertanyaan 8–13",
+          href: "/assesstment/bagian-1?input-8_comment=&input-9_comment=&input-10_comment=&input-11_comment=&input-12_comment=&input-13_comment=",
+        },
+        {
+          title: "Asesmen 3",
+          desc: "Pertanyaan 14–17",
+          href: "#",
+        },
+        {
+          title: "Asesmen 4",
+          desc: "Pertanyaan 18–21",
+          href: "#",
+        },
+      ],
+    },
+    {
+      title: "Bagian II",
+      subMenu: [
+        {
+          title: "Asesmen 1",
+          desc: "Pertanyaan 22–30",
+          href: "#",
+        },
+        {
+          title:
+            "Asesmen 2",
+          desc: "Pertanyaan 31–36",
+          href: "#",
+        },
+        {
+          title:
+            "Asesmen 3",
+          desc: "Pertanyaan 37–42",
+          href: "#",
+        },
+        {
+          title:
+            "Asesmen 4",
+          desc: "Pertanyaan 43–47",
+          href: "#",
+        },
+        {
+          title: "Asesmen 5",
+          desc: "Pertanyaan 48–52",
+          href: "#",
+        },
+        {
+          title: "Asesmen 6",
+          desc: "Pertanyaan 53–57",
+          href: "#",
+        },
+        {
+          title: "Komponen PPAM 7: Layananminum kesehatan Balita",
+          desc: "Pertanyaan 58–62",
+          href: "#",
+        },
+        {
+          title: "Komponen PPAM 8: Layanan Minimum Kesehatan Lansia",
+          desc: "Pertanyaan 63-67",
+          href: "#",
+        },
+        {
+          title:
+            "Kegiatan prioritas lainnya: Perawatan aborsi yang aman sesuai dengan hukum yang berlaku",
+          desc: "Pertanyaan 68-73",
+          href: "#",
+        },
+      ],
+    },
+  ];
+
+  function Menu({title, subMenu=[{
+    title: 'Bagian'
+  }]}) {
+
+    const isActive = activeAccordion === title;
+
+    function Assesstment({ title, desc }) {
+      return (
+        <button className="flex h-full w-full active:bg-accent">
+          <a
+            href="#"
+            className={`mb-2 ${
+              activeSection === title
+                ? "bg-accent rounded-lg py-4 pl-4 text-white"
+                : ""
+            }`}
+            onClick={() => handleSectionClick(title)}
+          >
+            {title}
+          </a>
+        </button>
+      );
+    }
+
+    // console.log(subMenu)
+    return (
+      <div
+        className={`flex flex-col mt-10 w-10/12 cursor-pointer justify-center ${ isActive && ('Bagian 0' === activeAccordion) && (activeSection === null) ? ' bg-accent py-3 pl-3 rounded-lg text-white' : ''}`}
+        onClick={ () => toggleAccordion(title)}
+        // style={
+        //   bacg
+        // }
+      >
+        <div className={`flex items-center h-auto justify-between `}>
+          <div className="flex items-center h-full gap-x-2">
+            <FaCircleCheck size={24} color="#CCC" /> <span>{title}</span>
+          </div>
+          {title === 'Bagian 0' ? () => {} :  activeAccordion === title ? (
+            <IoIosArrowDown size={16} />
+          ) : (
+            <IoIosArrowForward size={16} />
+          )}
+        </div>
+        {activeAccordion === title && activeAccordion !== 'Bagian 0' && (
+          <div className="pl-6 mt-4">
+            {/* Tambahkan item assessment untuk Bagian 02 */}
+            { subMenu.length > 1 && subMenu.map( (item, index) => (
+              <Assesstment key={index} title={`Asesmen-${index + 1}`} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className="bg-white pl-[31.83px] pt-[32px] w-[296px] h-screen fixed">
@@ -50,176 +200,10 @@ const Sidebar = () => {
       </h1>
       <div className="mt-2 border-b-2 w-10/12 border-black"></div>
 
-      {/* Bagian 01 */}
-      <div
-        className="flex flex-col mt-10 w-10/12 cursor-pointer"
-        onClick={() => toggleAccordion("bagian01")}
-      >
-        <a href="/assessment/bagian-0" className="flex items-center gap-x-2">
-          <FaCircleCheck size={24} className="text-white" />{" "}
-          <span>Bagian 0</span>
-        </a>
-
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-x-2">
-            <FaCircleCheck size={24} /> <span>Bagian 01</span>
-          </div>
-          {activeAccordion === "bagian01" ? (
-            <IoIosArrowDown size={16} />
-          ) : (
-            <IoIosArrowForward size={16} />
-          )}
-        </div>
-        {activeAccordion === "bagian01" && (
-          <div className="pl-6 mt-4">
-            {/* Tambahkan item assessment untuk Bagian 01 */}
-            <div
-              className={`flex items-center justify-between mb-10 ${
-                activeSection === "assessment-1"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-1")}
-            >
-              <a href="/assessment/bagian-01/assessment-1" className="mb-2">
-                Assessment 1
-              </a>
-            </div>
-            <a
-              href="/assessment/bagian-01/assessment-2"
-              className={`mb-2 ${
-                activeSection === "assessment-2"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2")}
-            >
-              Assessment 2
-            </a>
-            <a
-              href="/assessment/bagian-01/assessment-3"
-              className={`mb-2 ${
-                activeSection === "assessment-3"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-3")}
-            >
-              Assessment 3
-            </a>
-            <a
-              href="/assessment/bagian-01/assessment-4"
-              className={`mb-2 ${
-                activeSection === "assessment-4"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-4")}
-            >
-              Assessment 4
-            </a>
-          </div>
-        )}
-      </div>
-
-      {/* Bagian 02 */}
-      <div
-        className="flex flex-col mt-10 w-10/12 cursor-pointer"
-        onClick={() => toggleAccordion("bagian02")}
-      >
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-x-2">
-            <FaCircleCheck size={24} color="#CCC" /> <span>Bagian 02</span>
-          </div>
-          {activeAccordion === "bagian02" ? (
-            <IoIosArrowDown size={16} />
-          ) : (
-            <IoIosArrowForward size={16} />
-          )}
-        </div>
-        {activeAccordion === "bagian02" && (
-          <div className="pl-6 mt-4">
-            {/* Tambahkan item assessment untuk Bagian 02 */}
-            <a
-              href="/assessment/bagian-2/assessment-1"
-              className={`mb-2 ${
-                activeSection === "assessment-2-1"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2-1")}
-            >
-              Assessment 1
-            </a>
-            <a
-              href="/assessment/bagian-2/assessment-2"
-              className={`mb-2 ${
-                activeSection === "assessment-2-2"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2-2")}
-            >
-              Assessment 2
-            </a>
-            <a
-              href="/assessment/bagian-2/assessment-3"
-              className={`mb-2 ${
-                activeSection === "assessment-2-3"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2-3")}
-            >
-              Assessment 3
-            </a>
-            <a
-              href="/assessment/bagian-2/assessment-4"
-              className={`mb-2 ${
-                activeSection === "assessment-2-4"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2-4")}
-            >
-              Assessment 4
-            </a>
-            <a
-              href="/assessment/bagian-2/assessment-5"
-              className={`mb-2 ${
-                activeSection === "assessment-2-5"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2-5")}
-            >
-              Assessment 5
-            </a>
-            <a
-              href="/assessment/bagian-2/assessment-6"
-              className={`mb-2 ${
-                activeSection === "assessment-2-6"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2-6")}
-            >
-              Assessment 6
-            </a>
-            <a
-              href="/assessment/bagian-2/assessment-7"
-              className={`mb-2 ${
-                activeSection === "assessment-2-7"
-                  ? "bg-accent rounded-lg py-4 pl-4 text-white"
-                  : ""
-              }`}
-              onClick={() => handleSectionClick("assessment-2-7")}
-            >
-              Assessment 7
-            </a>
-          </div>
-        )}
-      </div>
+      {/* Check */}
+      { menus.map( (item, index) => (
+        <Menu key={index} title={item.title} subMenu={item.subMenu} />
+      ))}
     </div>
   );
 };
