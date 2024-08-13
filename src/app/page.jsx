b5191ea19ef6c91e28b65884992143f92d14f0c8
biillskiii <muhamad.nabil20052003@gmail.com>
2024-08-13 13:35:33 +0700

    update route

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,6 @@
 import React from "react";
-import Input from "@/modules/components/input";
-import DatePicker from "@/components/datepicker";
-import Button from "@/modules/components/button";
+import Login from "@/modules/login";
 const page = () => {
-  return (
-    <div className="space-y-10 p-5">
-      <Input />
-      <DatePicker />
-      <Button />
-    </div>
-  );
+  return <Login />;
 };
 export default page;
